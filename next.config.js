2c5543c705242aa32fbd2debe28a0a186d7bb820
Raphael B. <rafaelbenitah@gmail.com>
2025-08-12 14:49:06 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 const path = require('path');
 module.exports = {
   typescript: {
-    ignoreBuildErrors: true
-  }, // TEMP
+    ignoreBuildErrors: true  // TEMPORAIRE pour débloquer
+  },
   eslint: {
-    ignoreDuringBuilds: true
-  }, // TEMP
+    ignoreDuringBuilds: true  // TEMPORAIRE pour débloquer
+  },
   webpack: (config) => {
     config.resolve.alias['@'] = path.resolve(__dirname, 'src');
     return config;
